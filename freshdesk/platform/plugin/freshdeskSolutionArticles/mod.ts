553965c9c2c58e93354f6de5ef1f3f94919734f4
hrkremer <91268936+hrkremer@users.noreply.github.com>
2022-01-19 10:07:57 -0500

    Update mod.ts

--- freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
+++ freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
@@ -1,14 +1,16 @@
-//declare variable
+// Declare variable
 declare const account: string;
 
-// Appends Account subdomain and article ID generate a full URL
+// Appends account subdomain and article ID generate a full URL
 export function createFreshdeskSolutionUrl(articleId: string) {
-    if (`${account}`.includes("freshdesk")) {  
-        var accountclean = `${account}`.split('.freshdesk')[0]
-        var url = accountclean + ".freshdesk.com/support/solutions/articles/" + articleId
+    if (`${account}`.includes(".freshdesk.com")) {  
+        var url = `${account}` + "/support/solutions/articles/" + articleId
         }   
-    else {
+        else if (`${account}`.includes(".")) {
         var url = `${account}` + "/support/solutions/articles/" + articleId    
         }
+        else {
+        var url = `${account}` + ".freshdesk.com/support/solutions/articles/" + articleId    
+        }
     return url
-    }
+}
