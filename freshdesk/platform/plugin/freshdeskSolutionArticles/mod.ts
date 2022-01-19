c3ce8602c146baaa9439e10d5a33c9ca524b6aca
hrkremer <91268936+hrkremer@users.noreply.github.com>
2022-01-18 20:33:37 -0500

    Update mod.ts

--- freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
+++ freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
@@ -7,8 +7,8 @@ export function createFreshdeskSolutionUrl(articleId: string) {
         var accountclean = `${account}`.split('.freshdesk')[0]
         var url = accountclean + ".freshdesk.com/support/solutions/articles/" + articleId
         }   
-        else {
+    else {
         var url = `${account}` + "/support/solutions/articles/" + articleId    
         }
     return url
-}
+    }
