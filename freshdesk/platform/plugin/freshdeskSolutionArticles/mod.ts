d615f3ee91d6e837ae6324a06159bd0eae758eef
hrkremer <91268936+hrkremer@users.noreply.github.com>
2022-01-18 20:33:16 -0500

    Update mod.ts
    accommodate multiple url formats

--- freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
+++ freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
@@ -1,9 +1,14 @@
-//declar variable
+//declare variable
 declare const account: string;
 
 // Appends Account subdomain and article ID generate a full URL
 export function createFreshdeskSolutionUrl(articleId: string) {
-    var accountclean = `${account}`.split('.freshdesk')[0]
-    var url = accountclean + ".freshdesk.com/support/solutions/articles/" + articleId
+    if (`${account}`.includes("freshdesk")) {  
+        var accountclean = `${account}`.split('.freshdesk')[0]
+        var url = accountclean + ".freshdesk.com/support/solutions/articles/" + articleId
+        }   
+        else {
+        var url = `${account}` + "/support/solutions/articles/" + articleId    
+        }
     return url
 }
