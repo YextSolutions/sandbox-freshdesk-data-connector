511a164dcda9d9ea72fef87d62ef3a3cfc1a9812
hrkremer <91268936+hrkremer@users.noreply.github.com>
2022-01-18 20:12:13 -0500

    updating plugin delimiter

--- freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
+++ freshdesk/platform/plugin/freshdeskSolutionArticles/mod.ts
@@ -3,7 +3,7 @@ declare const account: string;
 
 // Appends Account subdomain and article ID generate a full URL
 export function createFreshdeskSolutionUrl(articleId: string) {
-    var accountclean = `${account}`.split('.')[0]
+    var accountclean = `${account}`.split('.freshdesk')[0]
     var url = accountclean + ".freshdesk.com/support/solutions/articles/" + articleId
     return url
 }
